3b814fcc285ec2386e568525dc625c8265e04ab0
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-22 23:42:26 +0100

    added new task

--- api/tasks/index.php
+++ api/tasks/index.php
@@ -1,9 +1,23 @@
 <?php
 
+header('Content-Type: application/json');
+
 $data_path = __DIR__ . '/../../data/tasks.json';
 
 $json_data = file_get_contents($data_path);
 
-header('Content-Type: application/json');
+$tasks = $json_data;
 
-echo $json_data;
+$new_task = $_POST['task'] ?? '';
+
+if($new_task) {
+
+    $tasks = json_decode($tasks, true);
+
+    $tasks[] = ['id' => rand(5,100), 'text' => "$new_task", 'done' => false];
+
+    $tasks = json_encode($tasks);
+
+}
+
+echo $tasks;
