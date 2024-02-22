4579558130ea67ecfe623c60070586da12f71b5b
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-22 17:25:57 +0100

    added data

--- app/js/script.js
+++ app/js/script.js
@@ -2,5 +2,30 @@ console.log('Vue OK', Vue);
 
 // Attivo Vue
 const { createApp } = Vue;
-const app = createApp({});
+const app = createApp({
+    data: () => ({
+        tasks: [
+            {
+                "id": 1,
+                "text": "HTML",
+                "done": false
+            },
+            {
+                "id": 2,
+                "text": "CSS",
+                "done": false
+            },
+            {
+                "id": 3,
+                "text": "Responsive design",
+                "done": false
+            },
+            {
+                "id": 4,
+                "text": "Javascript",
+                "done": false
+            }
+        ]
+    }),
+});
 app.mount('#root');
